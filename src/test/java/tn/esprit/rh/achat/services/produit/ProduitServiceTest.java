d1ae23e7463244df78692966d4515afc8f8c87a7
rafedbenjeddou <99246628+rafedbenjeddou@users.noreply.github.com>
2023-11-06 18:52:55 +0100

    Update ProduitServiceTest.java

--- src/test/java/tn/esprit/rh/achat/services/produit/ProduitServiceTest.java
+++ src/test/java/tn/esprit/rh/achat/services/produit/ProduitServiceTest.java
@@ -23,28 +23,48 @@ public class ProduitServiceTest {
 
     @InjectMocks
     ProduitServiceImpl produitService;
+    Date dateCreation = new Date(); // Set an actual date here
+    Date dateDerniereModification = new Date(); // Set an actual date here
+    Produit produit = new Produit(1L,"code1", "Libellé du produit 1", 10.0f, dateCreation, dateDerniereModification);
+    Produit addProduit = new Produit(1L,"code1", "Libellé du produit 1", 10.0f, dateCreation, dateDerniereModification);
 
+    List<Produit> listProduits = new ArrayList<Produit>() {
+        {
+            add(new Produit(1L,"code1", "Libellé du produit 1", 10.0f, dateCreation, dateDerniereModification));
+        }
+    };
     @Test
     public void testRetrieveProduit() {
-        // Create a sample Produit
-        Date dateCreation = new Date(); // Set an actual date here
-        Date dateDerniereModification = new Date(); // Set an actual date here
-        Produit produit = new Produit("code1", "Libellé du produit 1", 10.0f, dateCreation, dateDerniereModification);
-
-        // Define the behavior of the repository's findById method
         Mockito.when(produitRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(produit));
-
-        // Call the retrieveProduit method with a sample ID
         Produit retrievedProduit = produitService.retrieveProduit(1L);
-
-        // Verify that the repository's findById method was called with the provided ID
         Mockito.verify(produitRepository, Mockito.times(1)).findById(1L);
-
-        // Check that the retrieved Produit matches the sample Produit
         Assertions.assertNotNull(retrievedProduit);
         Assertions.assertEquals("code1", retrievedProduit.getCodeProduit());
         Assertions.assertEquals("Libellé du produit 1", retrievedProduit.getLibelleProduit());
         Assertions.assertEquals(10.0f, retrievedProduit.getPrix());
-        // Add other assertions as needed for date attributes
     }
+
+    @Test
+    public void testRetrieveAllProduit(){
+        Mockito.when(produitRepository.findAll()).thenReturn(listProduits);
+        List<Produit> listProduits = produitService.retrieveAllProduits();
+        Assertions.assertEquals(1, listProduits.size());
+    }
+    @Test
+   public void testAddProduit() {
+    // Prepare a Facture object to save
+
+    // Define the behavior for the mocked repository when saving
+    Mockito.when(produitRepository.save((addProduit))).thenReturn(addProduit);
+
+    // Call the addFacture method
+    Produit addProduit1 = produitService.addProduit(addProduit);
+
+    Mockito.verify(produitRepository).save((addProduit));
+    // Assertions to check if the returned Facture matches the expected one
+    Assertions.assertEquals(addProduit, addProduit1);
+
+
+}
+
 }
